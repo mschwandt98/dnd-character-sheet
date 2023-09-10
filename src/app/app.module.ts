982ddbbde7377ff38ce9ef426f1aac0476ca6047
mschwandt98 <marian.schwandt@gmail.com>
2023-09-10 16:47:08 +0200

    PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { NgModule, isDevMode } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -56,7 +57,18 @@ import { SettingsComponent } from './options/settings/settings.component';
         OptionsComponent,
         SettingsComponent
     ],
-    imports: [AppRoutingModule, BrowserModule, FormsModule, TooltipModule],
+    imports: [
+        AppRoutingModule,
+        BrowserModule,
+        FormsModule,
+        TooltipModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        })
+    ],
     providers: [
         {
             provide: LocationStrategy,
